8c9027aea6c8c21ca3f8a9def64271be26072fcd
Arpit88324 <arpitwasnik25@gmail.com>
2026-07-26 21:55:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (35.05%) - LeetSync

---
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def runningSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        l = list()
+        s = 0
+
+        for i in range(len(nums)):
+            s += nums[i]
+            l.append(s)
+        return l
+
+        
